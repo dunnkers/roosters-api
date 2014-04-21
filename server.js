ec9aa3bac5bebf9c347ba351b1372cbd4c0ce6a9
Dunnkers <dunnkers@hotmail.com>
2014-04-21 04:56:30 +0200

    Add teachers to the models being updated

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 #!/bin/env node
 
-var express= require('express'),
+var express = require('express'),
 	app = express(),
 	adapter = require('./lib/mongodb_adapter'),
 	RSVP = require('rsvp'),
 	_ = require('lodash'),
 	StudentIndexModel = require('./lib/models/student_index_model'),
-	url = require('url');
+	TeacherIndexModel = require('./lib/models/teacher_index_model');
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
 	port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var model = new StudentIndexModel();
+var studentModel = new StudentIndexModel(),
+	teacherModel = new TeacherIndexModel();
 
 app.configure(function () {
 	app.use(function (req, res, next) {
@@ -25,20 +26,8 @@ app.configure(function () {
 	app.set('json spaces', 0);
 });
 
-configure(model.items, model.item, model.Item).then(function () {
-	return configure(model.schedules, model.schedule, model.Schedule);
-}).then(function () {
-	return adapter.loadCollection(model.scheduleRelations).then(function () {
-		app.get('/' + model.scheduleRelations, function (req, res) {
-			adapter.findOne(model.scheduleRelations).then(function (doc) {
-				delete doc._id;
-				res.json(doc);
-			}, function (error) {
-				console.error('oops!', error);
-				res.send(404);
-			});
-		});
-	});
+configureCollection(studentModel).then(function () {
+	return configureCollection(teacherModel);
 }).then(function () {
 	app.get('/*', function(req, res) {
 		res.sendfile('index.html', {
@@ -49,47 +38,66 @@ configure(model.items, model.item, model.Item).then(function () {
 	app.listen(port, ip, function () {
 		console.log('Listening on %s:%d ...', ip, port);
 	});
-}, function (error) {
-	console.error('oops!', error);
 });
 
-function configure (name, singular, docModel) {
-	console.log("Loading %s...", name);
-	return adapter.loadCollection(name).then(function (count) {
-		console.log('Loaded %d %s!\n', count, name);
-		
-		app.get('/' + name, function (req, res) {
-			var ids = req.query.ids;
-			ids = (_.isString(ids) ? ids.split(',') : ids || []).map(function (id) {
-				return Number(id) || id;
-			});
-			var query = ids.length ? {_id: {$in: ids}} : {};
+function configureCollection (model) {
 
-			adapter.find(name, query).then(function (docs) {
-				var root = {};
-				root[name] = docs.map(function (doc) {
-					return modify(doc, docModel);
+	function configure (name, singular, docModel) {
+		console.log("Loading %s...", name);
+		return adapter.loadCollection(name).then(function (count) {
+			console.log('Loaded %d %s!\n', count, name);
+			
+			app.get('/' + name, function (req, res) {
+				var ids = req.query.ids;
+				ids = (_.isString(ids) ? ids.split(',') : ids || []).map(function (id) {
+					return Number(id) || id;
 				});
-				res.json(root);
-			}, function (error) {
-				console.error('oops!', error);
-				res.send(404);
-			});
-		});
+				var query = ids.length ? {_id: {$in: ids}} : {};
 
-		app.get('/' + name + '/:id', function (req, res) {
-			adapter.findOne(name, Number(req.params.id)).then(function (doc) {
-				if (!doc) {
+				adapter.find(name, query).then(function (docs) {
+					var root = {};
+					root[name] = docs.map(function (doc) {
+						return modify(doc, docModel);
+					});
+					res.json(root);
+				}, function (error) {
+					console.error('oops!', error);
 					res.send(404);
-				}
-				var root = {};
-				root[singular] = modify(doc, docModel);
-				res.json(root);
-			}, function (error) {
-				console.error('oops!', error);
-				res.send(404);
+				});
+			});
+
+			app.get('/' + name + '/:id', function (req, res) {
+				adapter.findOne(name, Number(req.params.id) || req.params.id).then(function (doc) {
+					if (!doc) {
+						res.send(404);
+					}
+					var root = {};
+					root[singular] = modify(doc, docModel);
+					res.json(root);
+				}, function (error) {
+					console.error('oops!', error);
+					res.send(404);
+				});
 			});
 		});
+	}
+	
+	return configure(model.items, model.item, model.Item).then(function () {
+		return configure(model.schedules, model.schedule, model.Schedule);
+	}).then(function () {
+		return adapter.loadCollection(model.scheduleRelations).then(function () {
+			app.get('/' + model.scheduleRelations, function (req, res) {
+				adapter.findOne(model.scheduleRelations).then(function (doc) {
+					delete doc._id;
+					res.json(doc);
+				}, function (error) {
+					console.error('oops!', error);
+					res.send(404);
+				});
+			});
+		});
+	}, function (error) {
+		console.error('oops!', error);
 	});
 }
 
--- update.js
+++ update.js
@@ -9,71 +9,90 @@ var format = require('util').format,
 var app = process.env.OPENSHIFT_APP_NAME;
 var grab = process.argv[2];
 
-var model = new StudentIndexModel(),
-	controller = new IndexController(model);
+var studentModel = new StudentIndexModel(),
+	studentController = new IndexController(studentModel);
+var teacherModel = new TeacherIndexModel(),
+	teacherController = new IndexController(teacherModel);
 
-handleCollection(model.items, function () {
-	return controller.authenticate(model.menuURL, 'menu').then(function (data) {
-		return controller.parseMenu(data);
-	});
-}).then(function (docs) {
-	console.log('');
-	return handleCollection(model.schedules, function () {
-		return controller.downloadSchedules(app ? docs : _.first(docs, 1));
-	}, docs);
-}).then(function () {
-	console.log('\nSetting %s schedule relations...', model.items);
-	return adapter.loadCollection(model.scheduleRelations).then(function () {
-		return adapter.remove(model.scheduleRelations, {});
-	});
-}).then(function () {
-	return adapter.findOne(model.schedules).then(function (doc) {
-		return adapter.resolveSchedule(model.schedules, _.first(doc.timetable)).then(function (doc) {
-			return adapter.insert(model.scheduleRelations, { relations: doc });
-		});
-	});
-}).then(function () {
-	console.log('Set %s schedule relations!', model.items);
+/*handleCollection(studentModel, studentController).then(function () {
+	return handleCollection(teacherModel, teacherController);
+}).then(function () {*/
+
+handleCollection(teacherModel, teacherController).then(function () {
 
 	adapter.close();
+	console.log('All finished!');
 });
 
+function handleCollection (model, controller) {
+	
+	function handle (name, download, items) {
+		var localModels;
 
-function handleCollection (name, download, items) {
-	var localModels;
+		console.log('[%s]', name.toUpperCase());
+		console.log("Loading %s...", name);
+		return adapter.loadCollection(name).then(function (count) {
+			var action = count > 0 ? format('Loaded %d', count) : 'Created collection';
+			console.log('%s %s!\n', action, name);
 
-	console.log('[%s]', name.toUpperCase());
-	console.log("Loading %s...", name);
-	return adapter.loadCollection(name).then(function (count) {
-		var action = count > 0 ? format('Loaded %d', count) : 'Created collection';
-		console.log('%s %s!\n', action, name);
+			console.log('Downloading %s...', name);
+			console.time('Download ' + name);
+		}).then(grab === '--nograb' ? function () {
+			return [];
+		} : download).then(function (models) {
+			console.timeEnd('Download ' + name);
+			console.log('Downloaded %d %s!\n', models.length, name);
+			localModels = models;
 
-		console.log('Downloading %s...', name);
-		console.time('Download ' + name);
-	}).then(grab === '--nograb' ? function () {
-		return [];
-	} : download).then(function (models) {
-		console.timeEnd('Download ' + name);
-		console.log('Downloaded %d %s!\n', models.length, name);
-		localModels = models;
+			console.log('Adding new %s...', name);
+			return adapter.addModels(name, models);
+		}).then(function (stats) {
+			console.log('Updated %d and inserted %d %s!\n', 
+				stats.updated || 0, stats.inserted || 0, name);
 
-		console.log('Adding new %s...', name);
-		return adapter.addModels(name, models);
-	}).then(function (stats) {
-		console.log('Updated %d and inserted %d %s!\n', stats.updated || 0, stats.inserted || 0, name);
+			console.log('Archiving old %s...', name);
+			return adapter.archiveOldModels(name, items || localModels);
+		}).then(function (count) {
+			console.log('Archived %d old %s!', count, name);
 
-		console.log('Archiving old %s...', name);
-		return adapter.archiveOldModels(name, items || localModels);
-	}).then(function (count) {
-		console.log('Archived %d old %s!', count, name);
+			console.log('[/%s]', name.toUpperCase());
+			return localModels;
+		}, function (error) {
+			console.error(error);
+		});
+	}
 
-		console.log('[/%s]', name.toUpperCase());
-		return localModels;
+	return handle(model.items, function () {
+		return controller.authenticate(model.menuURL, 'menu').then(function (data) {
+			return controller.parseMenu(data);
+		});
+	}).then(function (docs) {
+		console.log('');
+		return handle(model.schedules, function () {
+			return controller.downloadSchedules(app ? docs : _.first(docs, 1));
+		}, docs);
+	}).then(function () {
+		console.log('\nSetting %s schedule relations...', model.items);
+		return adapter.loadCollection(model.scheduleRelations).then(function () {
+			return adapter.remove(model.scheduleRelations, {});
+		});
+	}).then(function () {
+		return adapter.findOne(model.schedules).then(function (doc) {
+			return adapter.resolveSchedule(model.schedules, _.first(doc.timetable)).then(function (doc) {
+				return adapter.insert(model.scheduleRelations, { relations: doc });
+			});
+		});
+	}).then(function () {
+		console.log('Set %s schedule relations!', model.items);
 	}, function (error) {
 		console.error(error);
 	});
 }
 
+
+
 // if rooster reverted, it wont update
 // headers give lastModified information
 // modular schedule downloads by saving them when they arrive
+// maybe make unified handle method for both server and update, which loads
+// 		the collection. use middleware in then clause for this.

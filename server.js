1130922496fd5b15ae70f2365dfc9b0009480cea
Dunnkers <dunnkers@hotmail.com>
2014-10-07 19:24:09 +0200

    Support nested population for non-lean documents!

--- server.js
+++ server.js
@@ -131,55 +131,67 @@ function cleanNulls (object) {
 }
 
 /**
- * Populates the invoked document(s) with as the given model.
+ * Populates the invoked document(s) with as the given model. If you don't
+ * pass `lean: true` as an option, documents with 'missing' references will
+ * have null values.
  * 
  * @param  {Model} model  The model of the docs.
  * @param  {[Model]} models  An array of previous models. Necessary
  * to avoid circular references caused by recursion.
  * @return {Promise}  A promise containing (recursed) population.
  */
-function populate (model, models) {
+function populate (model, options, models) {
 	models = models || [];
-	models.push(model.modelName);
+	options = options || {};
+
+	// avoid following circular references by keeping a register of (parent) models
+	if (!_.contains(models, model.modelName)) models.push(model.modelName);
 
 	// docs is either Array or Object
 	return function (docs) {
 		var paths = populatePaths(model);
 
-		// filter paths to populate.
+		// filter paths to populate
 		paths = _.transform(paths, function (res, model, path) {
 			// only populate fields that actually exist
 			var exists = _.isArray(docs) || docs[path];// arr ? _.some(docs, path)
 
 			// don't populate previously populated models
-			if (!_.contains(models, model.modelName) && exists) {
-				res[path] = model;
-			}
+			if (!_.contains(models, model.modelName) && exists) res[path] = model;
 		});
 
-		var path = _.keys(paths).join(' '),
-			options = { lean: true };
-
 		if (_.isEmpty(paths)) return docs;
+
+		var path = _.keys(paths).join(' ');
 
 		return model.populate(docs, {
 			path: path,
 			options: options
 		}).then(function (docs) {
 			function recurse (doc) {
-				// remove padded null values caused when populating an array of docs
-				doc = _.isArray(doc) ? doc.map(cleanNulls) : cleanNulls(doc);
+				// doc is lean, we can alter null values
+				if (!doc.toJSON) {
+					// remove null values padded when population failed.
+					doc = _.isArray(doc) ? doc.map(cleanNulls) : cleanNulls(doc);
+				}
 
-				_.forIn(paths, function (value, key) {
-					if (!_.isUndefined(doc[key])) {
+				// create hash of fields to populate (not hashing doc to retain prototype)
+				return RSVP.hash(_.transform(paths, function (res, model, path) {
+					// if ref was padded as null or somehow became undefined
+					if (!_.isUndefined(doc[path]) && !_.isNull(doc[path])) {
 						// recursively search for more fields to populate
-						doc[key] = populate(value, models)(doc[key]);
+						res[path] = populate(model, options, models)(doc[path]);
 					}
+				})).then(function (populated) {
+					// attach populated fields to doc
+					_.forIn(populated, function (value, path) {
+						doc[path] = value;
+					});
+
+					return doc;
 				});
-				return RSVP.hash(doc);
 			}
 
-			// now check populated props for population
 			return _.isArray(docs) ? RSVP.all(docs.map(recurse)) : recurse(docs);
 		});
 	};

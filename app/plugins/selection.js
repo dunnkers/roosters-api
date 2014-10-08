c008ed0a7e1616a3edfe8aebdef5d3a1eac52ea5
Dunnkers <dunnkers@hotmail.com>
2014-10-08 13:37:31 +0200

    Fix.

--- app/plugins/selection.js
+++ app/plugins/selection.js
@@ -2,17 +2,17 @@ var _ = require('lodash');
 
 module.exports = function (schema) {
 	// unless overwritten, set defaults for a select option in this models' populations.
-	var str = '-__v -createdAt -updatedAt -index',
+	var str = '-__v -createdAt -updatedAt ',
 		select = schema.options.select;
 
 	var res = { self: str, population: str };
 
 	if (select)
 		if (_.isString(select)) {
-			res.self += ' ' + select;
+			res.self += select;
 		} else if (_.isObject(select)) {
 			res.population =  select.population || res.self;
-			if (select.self) res.self += ' ' + select.self;
+			if (select.self) res.self += select.self;
 		}
 
 	if (!schema.options.selection)

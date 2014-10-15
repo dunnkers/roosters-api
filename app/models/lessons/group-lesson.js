fb4f2030e62300a301903432b94e88490a0165c6
Dunnkers <dunnkers@hotmail.com>
2014-10-15 16:30:00 +0200

    Update group-lesson back to correct indexes.

--- app/models/lessons/group-lesson.js
+++ app/models/lessons/group-lesson.js
@@ -4,9 +4,9 @@ var LessonSchema = require('../lesson'),
 var Schema = new LessonSchema();
 
 Schema.virtual('content').set(function (content) {
-	if (content[1]) this.subject = content[0];
-	if (content[3]) this.teacher = content[1];
-	if (content[4]) this.room = content[2];
+	if (content[0]) this.subject = content[0];
+	if (content[1]) this.teacher = content[1];
+	if (content[2]) this.room = content[2];
 });
 
 Lesson.discriminator('GroupLesson', Schema);
